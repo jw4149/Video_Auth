1653cdcd3837c58242f1eb9f07a1b29dd9f08f4b
ultraspicy <element.no.88@gmail.com>
2024-01-03 22:43:33 -0800

    Print out some metadata of video stream, update readme

--- video/README.md
+++ video/README.md
@@ -8,3 +8,11 @@
 ### Output path
  - <ROOT>/video/fixtures/blackbox/frame{}.mp4 (fixtures are gitignored to make git faster)
 
+
+### Useful links
+ - https://docs.rs/ffmpeg-next/6.1.0/ffmpeg_next/format/stream/struct.Stream.html#method.frames for rust.doc of stream 
+
+### Dan's question
+ - when add overlay, does mp4 file have different video streams or just a (combined) stream? 
+    - A combined stream
+
--- video/src/file_op.rs
+++ video/src/file_op.rs
@@ -4,12 +4,13 @@ use ffmpeg::format::Pixel;
 use ffmpeg::util::frame::video::Video;
 use std::fs::File;
 use std::io::Write;
-
+use std::any::type_name;
 
 pub fn save_file(frame: &Video, index: usize) -> std::result::Result<(), std::io::Error> {
-    let mut file = File::create(format!("./fixtures/blackbox/frame{}.ppm", index))?;
+    let mut file = File::create(format!("./fixtures/overlay/frame{}.ppm", index))?;
     file.write_all(format!("P6\n{} {}\n255\n", frame.width(), frame.height()).as_bytes())?;
     file.write_all(frame.data(0))?;
+    //println!("{}", frame.data(0).len());
     Ok(())
 }
 
@@ -29,4 +30,8 @@ pub fn draw_black_box(frame: &mut Video, x: usize, y: usize, width: usize, heigh
             frame.data_mut(0)[pixel_index + 2] = 0; // B
         }
     }
+}
+
+pub fn print_type_of<T>(_: &T) {
+    println!("{}", type_name::<T>());
 }
