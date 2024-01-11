f900ddc7d7442f964e09aea27ef68e4d5a302126
ultraspicy <element.no.88@gmail.com>
2024-01-10 23:22:36 -0800

    loop thru all input files in the folder

--- image_compare/src/main.rs
+++ image_compare/src/main.rs
@@ -1,83 +1,56 @@
 use image::{open, GenericImageView};
+use image::{DynamicImage, ImageBuffer, Rgba};
 use plotters::prelude::*;
 use std::error::Error;
 use std::process;
+use std::fs::File;
+use std::io::{Result, Write};
 
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let container1 = load_image("original.ppm");
-    let container2 = load_image("edited.ppm");
+fn main() -> Result<()> {
 
-    // Print the vector
-    println!("width, height of the first image {:?}, {:?}", container1.width, container1.height);
-    println!("width, height of the second image {:?}, {:?}", container2.width, container2.height);
+    for i in 0..256 {
+        let original_file_path = format!("output_original_frames/frame{}.ppm", i);
+        let output_file_path = format!("output_frames/frame{}.ppm", i);
+        let container1 = load_image(&original_file_path);
+        let container2 = load_image(&output_file_path);
 
-    if container1.width != container2.width || container1.height != container2.height {
-        println!("The dimension of two images are not the same, cannot build historgram");
-        process::exit(1);
+        // Print the vector
+        println!("width, height of the first image {:?}, {:?}", container1.width, container1.height);
+        println!("width, height of the second image {:?}, {:?}", container2.width, container2.height);
+
+        if container1.width != container2.width || container1.height != container2.height {
+            println!("The dimension of two images are not the same, cannot build historgram");
+            process::exit(1);
+        }
+
+        let delta_vec = compare_diff_abs(container1.data.as_ref(), container2.data.as_ref());
+
+        // Split the data into four channels
+        let mut rgba_channels = (Vec::new(), Vec::new(), Vec::new(), Vec::new());
+
+        let output_vec_file = format!("diff_vecs/diff_{}.txt", i);
+        write_pixel_data_to_file(&delta_vec, &output_vec_file);
+
+        for [c1, c2, c3, c4] in delta_vec {
+            rgba_channels.0.push(c1);
+            rgba_channels.1.push(c2);
+            rgba_channels.2.push(c3);
+            rgba_channels.3.push(c4);
+        }
+
+        let diff_r_max = *rgba_channels.0.iter().max().unwrap() as i32;
+        let diff_g_max = *rgba_channels.1.iter().max().unwrap() as i32;
+        let diff_b_max = *rgba_channels.2.iter().max().unwrap() as i32;
+        let diff_a_max = *rgba_channels.3.iter().max().unwrap() as i32;
+
+        // debug log 
+        println!("max diff for r,g,b,a channels {:?}, {:?}, {:?}, {:?}", diff_r_max, diff_g_max, diff_b_max, diff_a_max);
     }
-
-    let delta_vec = compare_diff_abs(container1.data.as_ref(), container2.data.as_ref());
-
-    // Split the data into four channels
-
-    let mut rgba_channels = (Vec::new(), Vec::new(), Vec::new(), Vec::new());
-    for [c1, c2, c3, c4] in delta_vec {
-        rgba_channels.0.push(c1);
-        rgba_channels.1.push(c2);
-        rgba_channels.2.push(c3);
-        rgba_channels.3.push(c4);
-    }
-
-    let diff_r_max = *rgba_channels.0.iter().max().unwrap() as i32;
-    let diff_g_max = *rgba_channels.1.iter().max().unwrap() as i32;
-    let diff_b_max = *rgba_channels.2.iter().max().unwrap() as i32;
-    let diff_a_max = *rgba_channels.3.iter().max().unwrap() as i32;
-
-    // debug log 
-    println!("max diff for r,g,b,a channels {:?}, {:?}, {:?}, {:?}", diff_r_max, diff_g_max, diff_b_max, diff_a_max);
-
-
-    // Define dimensions for the histograms
-    //let dimensions = (container1.width, container1.height);
-
-    // // Create histograms for each channel
-    // for (i, channel_data) in rgba_channels.into_iter().enumerate() {
-    //     let filename = format!("histogram_channel_{}.png", i + 1);
-    //     create_histogram(&filename, &channel_data, dimensions)?;
-    // }
-
-    // let filename = format!("histogram_channel_1.png");
-    // create_histogram(&filename, &rgba_channels.0)?;
 
     Ok(())
 
 }
-
-// fn create_histogram(filename: &str, data: &Vec<i16>) -> Result<(), Box<dyn std::error::Error>> {
-//     let root = BitMapBackend::new(filename, (3840, 2160)).into_drawing_area();
-//     root.fill(&WHITE)?;
-
-//     let max_y = *data.iter().max().unwrap() as i32;
-//     println!("max_y = {}", max_y);
-    
-//     let mut chart = ChartBuilder::on(&root)
-//         .caption("Line Plot", ("sans-serif", 50))
-//         .x_label_area_size(3840)
-//         .y_label_area_size(2160)
-//         .build_cartesian_2d(0..data.len() as i32, 0..max_y)?;
-
-//     chart.configure_mesh().draw()?;
-
-//     chart.draw_series(LineSeries::new(
-//         data.iter().enumerate().map(|(x, &y)| (x as i32, y as i32)), 
-//         &RED,
-//     ))?;
-
-//     root.present()?;
-
-//     Ok(())
-// }
 
 fn compare_diff_abs(a: &Vec<[u8; 4]>, b: &Vec<[u8; 4]>) -> Vec<[i16;4]> {
    a.iter().zip(b.iter()).map(|(a_elem, b_elem)| {
@@ -99,7 +72,7 @@ fn load_image(path: &str) -> ImageContainer {
     for y in 0..height {
         for x in 0..width {
             let mut pixel = img.get_pixel(x, y).0; // Get the pixel value as RGBA
-            if (x >= 500 && x < 600 && y >= 500 && y < 600) {
+            if x >= 500 && x < 600 && y >= 500 && y < 600 {
                 pixel = [0u8; 4];
             }
             img_vec.push(pixel);
@@ -112,6 +85,17 @@ fn load_image(path: &str) -> ImageContainer {
         data: img_vec,
     };
     ret
+}
+
+fn write_pixel_data_to_file(data: &Vec<[i16; 4]>, file_path: &str) {
+    let mut file = File::create(file_path).unwrap();
+
+    for arr in data {
+        let line = format!("{:?}\n", arr);
+        file.write_all(line.as_bytes()).unwrap();
+    }
+
+    println!("Data has been written to {}", file_path);
 }
 
 struct ImageContainer {
